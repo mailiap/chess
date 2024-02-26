6a3512965dc4f1f06516bd1fb8c4c2655829f4de
mailiap <mailiap@byu.edu>
2024-02-26 16:10:50 -0700

    server working(phase3)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,15 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
+import java.util.*;
 
 public class Server {
+    private ArrayList<String> names=new ArrayList<>();
+
+    public static void main(String[] args) {
+        new Server().run(8080);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,6 +17,15 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clearApplication);
+        Spark.post("/user", this::register);
+//        Spark.post("/user", (req, res) -> (new register()).handleRequest(req, res));
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGames);
+        Spark.put("/game", this::joinGame);
+//        Spark.delete("/name/:name", this::deleteName);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -17,6 +33,40 @@ public class Server {
 
     public void stop() {
         Spark.stop();
-        Spark.awaitStop();
+    }
+
+    private Object clearApplication(Request req, Response res) {
+        names.remove(req.params(":name"));
+        return clearApplication(req, res);
+    }
+
+    private Object register(Request req, Response res) {
+        names.add(req.params(":name"));
+        return register(req, res);
+    }
+
+    private Object login(Request req, Response res) {
+        names.add(req.params(":name"));
+        return login(req, res);
+    }
+
+    private Object logout(Request req, Response res) {
+        names.remove(req.params(":name"));
+        return logout(req, res);
+    }
+
+    private Object listGames(Request req, Response res) {
+        res.type("application/json");
+        return new Gson().toJson(Map.of("name", names));
+    }
+
+    private Object createGames(Request req, Response res) {
+        names.add(req.params(":name"));
+        return createGames(req, res);
+    }
+
+    private Object joinGame(Request req, Response res) {
+        names.add(req.params(":name"));
+        return joinGame(req, res);
     }
 }
